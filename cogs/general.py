cf4788942272fee8cecf363cb57ca46ab371f047
ItzSomeGuy <a.floyd135@gmail.com>
2021-06-06 08:22:21 -0400

    - fixed rename command command renamed target <id + nickname>

--- cogs/general.py
+++ cogs/general.py
@@ -1,3 +1,5 @@
+import random
+
 import discord
 from discord.ext import commands
 
@@ -5,6 +7,31 @@ from discord.ext import commands
 class General(commands.Cog):
     def __init__(self, client):
         self.client = client
+
+    @commands.command(aliases=["8ball"])
+    async def _8ball(self, ctx, *, question):
+        responses = ["It is certain.",
+                     "It is decidedly so.",
+                     "Without a doubt.",
+                     "Yes - definitely.",
+                     "You may rely on it.",
+                     "As I see it, yes.",
+                     "Most likely.",
+                     "Outlook good.",
+                     "Yes.",
+                     "Signs point to yes.",
+                     "Reply hazy, try again.",
+                     "Ask again later.",
+                     "Better not tell you now.",
+                     "Cannot predict now.",
+                     "Concentrate and ask again.",
+                     "Don't count on it.",
+                     "My reply is no.",
+                     "My sources say no.",
+                     "Outlook not so good.",
+                     "Very doubtful."]
+
+        await ctx.send(f"Question: {question}\nAnswer: {random.choice(responses)}")
 
     @commands.command()
     async def ban(self, ctx):
@@ -28,21 +55,28 @@ class General(commands.Cog):
         embed.set_author(name="Help")
 
         # embed.add_field(name="", value="", inline=False)
-        # embed.add_field(name="8ball [question]", value="does some spooky shit", inline=False)
+        embed.add_field(name="8ball [question]", value="does some spooky shit", inline=False)
         embed.add_field(name="ban [member]", value="beans someone", inline=False)
         embed.add_field(name="hello", value="used as a greeting or to begin a telephone conversation", inline=False)
         embed.add_field(name="help", value="this..", inline=False)
-        # embed.add_field(name="insult [member]", value="use it to help win arguments", inline=False)
         embed.add_field(name="rename [member] [nickname]", value="use to rename a member", inline=False)
         embed.add_field(name="stalk [member]", value="fetches avatar of someone", inline=False)
 
         await ctx.send(embed=embed)
 
     @commands.command()
-    async def rename(self, ctx):
+    async def rename(self, ctx, target, *args):
         await ctx.message.delete()
 
         target = ctx.message.mentions[0]
+
+        new_name = ""
+
+        if args is not None:
+            new_name = " ".join(args[:])
+
+        await target.edit(nick=new_name)
+        print(f"renamed {target.name} to {new_name}")
 
     @commands.command()
     async def stalk(self, ctx, target):
